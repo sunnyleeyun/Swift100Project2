95778ce973cd6152b2c549fe5feb03dd7a174d35
Sunny <sunnylee945@hotmail.com>
2017-10-18 18:55:03 +0800

    delete unneeded

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -9,10 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  
+  @IBOutlet var longPress_Outlet: UILongPressGestureRecognizer!
+  
+  @IBAction func longPress_Button_Tapped(_ sender: Any) {
+    print("long press")
+    num += 1
+    number.text = String(num)
+  }
+  
+  var num: Int = 0
+  @IBOutlet weak var number: UILabel!
+  @IBOutlet weak var TaporHold_Outlet: UIButton!
+  @IBAction func Reset_Button_Tapped(_ sender: Any) {
+    num = 0
+    number.text = String(num)
+  }
+  @IBAction func TaporHold_Button_Tapped(_ sender: Any) {
+    num += 1
+    number.text = String(num)
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+    navigationController?.navigationBar.shadowImage = UIImage()
+    
+    number.text = String(num)
+    
+    TaporHold_Outlet.addGestureRecognizer(longPress_Outlet)
+    
+    
   }
 
   override func didReceiveMemoryWarning() {
